93784fdf491d73df0893bfba0d411bc2e567a29c
Deshan Nawanjana <deshan.uok@gmail.com>
2024-10-09 14:10:58 +0530

    Raycaster Events and Texture Update

--- src/canvas/Canvas.js
+++ src/canvas/Canvas.js
@@ -14,18 +14,24 @@ const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000)
 const renderer = new THREE.WebGLRenderer({ alpha: true })
 
+// raycaster modules
+const raycaster = new THREE.Raycaster()
+const mouse = new THREE.Vector2()
+
 // method to load diamond models
 const loadDiamonds = async () => {
   // load diamond model
-  const model = await new FBXLoader().loadAsync("./models/diamond.fbx")
+  const model = await new FBXLoader().loadAsync("./models/diamond_v3.fbx")
+  // load diamond texture
+  const texture = await new THREE.TextureLoader().loadAsync("./models/diamond_v3.png")
   // load reflection texture
-  const texture = await new THREE.CubeTextureLoader().loadAsync([
-    "./models/diamond_hdr.jpg",
-    "./models/diamond_hdr.jpg",
-    "./models/diamond_hdr.jpg",
-    "./models/diamond_hdr.jpg",
-    "./models/diamond_hdr.jpg",
-    "./models/diamond_hdr.jpg"
+  const envTexture = await new THREE.CubeTextureLoader().loadAsync([
+    "./models/skybox_v2.png",
+    "./models/skybox_v2.png",
+    "./models/skybox_v2.png",
+    "./models/skybox_v2.png",
+    "./models/skybox_v2.png",
+    "./models/skybox_v2.png"
   ])
   // set model scale
   model.scale.set(0.15, 0.15, 0.15)
@@ -39,14 +45,14 @@ const loadDiamonds = async () => {
     diamond.traverse(child => {
       // update model material
       child.material = new THREE.MeshPhysicalMaterial({
-        envMap: texture,
-        color: new THREE.Color(0x999999),
-        metalness: 0.2,
-        roughness: 0.1,
+        map: texture,
+        envMap: envTexture,
+        color: new THREE.Color("#FFFFFF"),
+        metalness: 0,
+        roughness: 0.01,
         transparent: true,
-        opacity: 0.85,
-        transmission: 0.9,
-        ior: 2.4,
+        opacity: 1,
+        transmission: 0.5,
         reflectivity: 1,
         clearcoat: 1,
         clearcoatRoughness: 0.05,
@@ -59,12 +65,23 @@ const loadDiamonds = async () => {
     // locate and rotate diamond
     diamond.position.set(...Object.values(item.anchor.position))
     diamond.rotation.set(...Object.values(item.anchor.rotation))
+    // get mesh parts
+    diamond.top = diamond.children[0]
+    diamond.bottom = diamond.children[1]
+    diamond.full = diamond.children[2]
+    // set full diamond flag
+    diamond.full.isFullDiamond = true
+    diamond.full.name = "Diamond_" + i
+    // hide broken diamond parts
+    diamond.top.visible = false
+    diamond.bottom.visible = false
     // set model on item
     item.model = diamond
     // create tween for model
     item.tween = new Tween()
     // set index on model
-    item.model.index = i
+    item.model.full.index = i
+    item.model.full.color = item.color
   }
   // return diamonds array
   return items.map(item => item.model)
@@ -73,21 +90,17 @@ const loadDiamonds = async () => {
 // method to load lights
 const loadLights = () => {
   // create ambient light
-  const ambient = new THREE.AmbientLight(0xffffff, 0.4)
+  const ambient = new THREE.AmbientLight(0xffffff, 5)
   // create directional light
   const directional = new THREE.DirectionalLight(0xffffff, 1)
   directional.position.set(5, 10, 7.5)
   directional.castShadow = true
-  // create point light
-  const point = new THREE.PointLight(0xff9900, 1, 2)
-  point.position.set(-5, 5, 5)
-  point.castShadow = true
   // return all lights
-  return [ambient, directional, point]
+  return [ambient, directional]
 }
 
 // method to initiate
-const onInit = async () => {
+const onInit = async (index, setIndex) => {
   // load and add diamond items
   scene.add(...await loadDiamonds())
   // load and add lights
@@ -106,6 +119,27 @@ const onInit = async () => {
     // render scene
     renderer.render(scene, camera)
   })
+  // mouse click event
+  window.addEventListener("click", event => {
+    // calculate points from pixel ratio
+    const x = event.clientX * window.devicePixelRatio
+    const y = event.clientY * window.devicePixelRatio
+    // update mouse vector
+    mouse.x = (x / renderer.domElement.width) * 2 - 1
+    mouse.y = -(y / renderer.domElement.height) * 2 + 1
+    // set raycaster from camera
+    raycaster.setFromCamera(mouse, camera)
+    // get input meshes
+    const inputs = items.map(item => item.model.full)
+    // get intersect objects
+    const objects = raycaster.intersectObjects(inputs)
+    // find full diamond mesh
+    const result = objects.find(item => item.object.isFullDiamond)
+    // return if no result
+    if (!result) { return }
+    // set diamond index
+    setIndex(items.length - result.object.index)
+  }, false)
   // initial resize
   onResize()
 }
@@ -136,24 +170,24 @@ const rotateDiamonds = index => {
     const offsetItem = items[offsetIndex]
     // animate model
     item.tween.animateObject(item.model, offsetItem.anchor, {
-      duration: 500,
-      easing: 'out-quad',
+      duration: 800,
+      easing: 'out-cubic',
       onUpdate() {
         // get child mesh
-        const mesh = item.model.children[0]
-        const posX = Math.abs(item.model.position.x)
+        const mesh = item.model
+        const posX = item.model.position.x
         const posZ = item.model.position.z
-        if (posZ < - 35 && posX < 32) {
-          mesh.material.opacity = posX / 220
+        if (posZ < -34 && (posX < 38 && posX > -38)) {
+          mesh.visible = false
         } else {
-          mesh.material.opacity = 1
+          mesh.visible = true
         }
       }
     })
   }
 }
 
-export default function Canvas({ ready, index, setReady }) {
+export default function Canvas({ ready, setReady, index, setIndex }) {
   // effect on mount
   useEffect(() => {
     // get container element
@@ -165,7 +199,7 @@ export default function Canvas({ ready, index, setReady }) {
       // set as not ready
       setReady(false)
       // initiate module
-      onInit().then(() => {
+      onInit(index, setIndex).then(() => {
         // set as ready
         setReady(true)
       })
@@ -180,7 +214,7 @@ export default function Canvas({ ready, index, setReady }) {
   // effect on index change
   useEffect(() => {
     // relocate diamonds by index offset
-    if (ready) { rotateDiamonds(index, setReady) }
+    if (ready) { rotateDiamonds(index) }
   }, [index])
   // return container
   return <div className="canvas-container" />
